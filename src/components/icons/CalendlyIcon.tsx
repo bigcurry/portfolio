7a676952f38069f566ecac6973475e591f22d235
Suraj Pabba <suraj.pabba89@gmail.com>
2024-02-01 14:44:40 -0600

    c

--- src/components/icons/CalendlyIcon.tsx
+++ src/components/icons/CalendlyIcon.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import CalendlyLogo from '/path/to/calendly-logo.svg'; // Adjust the path accordingly
+
 export const CalendlyIcon = (props: any) => {
   return <img src="/calendly-logo.svg" alt="Calendly Logo" {...props} />;
 };
